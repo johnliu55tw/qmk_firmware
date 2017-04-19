24e39b273e2fa45be933dcf67567f0fe74c8caf4
John Liu <johnliu55tw@gmail.com>
2017-04-19 10:55:48 +0800

    Add xd60-mine keymap

---
+++ keyboards/xd60/keymaps/mine/keymap.c
@@ -0,0 +1,35 @@
+#include "xd60.h"
+#include "action_layer.h"
+
+
+// Layer name aliasing
+#define BASE 0
+#define FN   1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // 0: Base Layer
+  [BASE] = KEYMAP(
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,     \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSLS,   \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_NO,   KC_RSFT,  TG(FN),    \
+      KC_GRV,  MO(FN),  KC_LALT,                          KC_SPC,                          KC_NO,   MO(FN),   KC_LEFT, KC_DOWN,  KC_RIGHT
+  ),
+  // 1: Function Layer
+  [FN] = KEYMAP(
+      KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_F13,   KC_DEL,    \
+      KC_NO,   KC_BTN1, KC_MS_U ,KC_BTN2, KC_WH_U,KC_HOME,KC_HOME,KC_NO,  KC_INS, KC_NO,   KC_PSCR, KC_SLCK,  KC_PAUS,           KC_TRNS,   \
+      KC_CAPS, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,KC_END, KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,KC_HOME, KC_PGUP,  KC_NO,             KC_ENT,    \
+      KC_LSFT, KC_NO,   KC_NO,   KC_APP,  BL_STEP,KC_NO,  KC_NO,  KC_END, KC_NO,  KC_NO,   KC_END,  KC_PGDN,  KC_RSFT, KC_PGUP,  KC_TRNS,   \
+      KC_LCTL, KC_TRNS, KC_LALT,                          KC_SPC,                          KC_RGUI, KC_TRNS,  KC_HOME, KC_PGDOWN,KC_END
+  ),
+};
+
+// Custom Actions
+const uint16_t PROGMEM fn_actions[] = {
+};
+
+// Loop
+void matrix_scan_user(void) {
+  // Empty
+};
