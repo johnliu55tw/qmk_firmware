0f8c83f56b182c5d5e377819ad19574d0b07dd6f
John Liu <liujoh@microsoft.com>
2023-09-03 00:11:48 +0800

    Copied info.json from redox

---
+++ keyboards/handwired/johnsplit/rules.mk
@@ -0,0 +1,16 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+
+SPLIT_KEYBOARD = yes
+
+# Disable unsupported hardware
+AUDIO_SUPPORTED = no
+BACKLIGHT_SUPPORTED = no
